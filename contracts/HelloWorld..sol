dae28195854418120932b1a464301e27559b4b08
"otinomz" <"awesomeotinomz@gmail.com">
2022-04-11 22:40:09 +0100

    feat: created a simple contract to display hello world and to also store and retrieve number

---
+++ contracts/HelloWorld..sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+/**
+ * @title Hello World simple program and a Store And Retrieve Unit Number Function
+ * @dev StoreNumber function to store an unsigned integer  
+ *      retrieveNumber function to retrieve the number.
+ */
+contract HelloWorld {
+    // display Hello World as string variable
+    string public displayHelloWorld = "Hello World";
+
+    uint number;
+    /**
+     * @dev Store value in variable
+     * @param num value to store
+     */
+    function storeNumber(uint num) public {
+        number = num;
+    }
+    /**
+     * @dev Retrieve value of unsigned integer value 
+     * @return value of 'number'
+     */
+    function retrieveNumber() public view returns (uint) {
+        return number;
+    }
+}
